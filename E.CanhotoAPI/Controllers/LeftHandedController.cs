5cdb648359cdea60655e180967736c3d33f528b3
Jean Andrade <jeancarlos.av0@gmail.com>
2024-06-10 21:16:28 -0300

    Adicionado tratamento de erro

--- E.CanhotoAPI/Controllers/LeftHandedController.cs
+++ E.CanhotoAPI/Controllers/LeftHandedController.cs
@@ -1,7 +1,6 @@
 ﻿using E.CanhotoAPI.DTO;
 using E.CanhotoAPI.Models;
 using E.CanhotoAPI.Repositorios.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E.CanhotoAPI.Controllers
@@ -20,40 +19,110 @@ namespace E.CanhotoAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<List<CanhotosResponse>>> BuscarCanhotos()
         {
-            List<CanhotosResponse> leftHandeds = await _leftHandedRepositorio.BuscarTodosOsCanhotos();
-            return Ok(leftHandeds);
+            try
+            {
+                List<CanhotosResponse> leftHandeds = await _leftHandedRepositorio.BuscarTodosOsCanhotos();
+                return Ok(leftHandeds);
+            }
+            catch (Exception ex)
+            {
+                // Log da exceção
+                Console.WriteLine($"Ocorreu um erro ao buscar todos os canhotos: {ex.Message}");
+
+                // Retorno de uma resposta de erro com status 500 (Internal Server Error)
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao buscar todos os canhotos. Por favor, tente novamente mais tarde.");
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<List<CanhotosResponse>>> BuscarPorId(int id)
         {
-            CanhotosResponse leftHanded = await _leftHandedRepositorio.BuscarPorId(id);
-            return Ok(leftHanded);
+            try
+            {
+                CanhotosResponse leftHanded = await _leftHandedRepositorio.BuscarPorId(id);
+
+                if (leftHanded == null)
+                {
+                    return NotFound("Canhoto não encontrado");
+                }
+
+                return Ok(leftHanded);
+            }
+            catch (Exception ex)
+            {
+                // Log da exceção
+                Console.WriteLine($"Ocorreu um erro ao buscar o canhoto com o ID {id}: {ex.Message}");
+
+                // Retorno de uma resposta de erro com status 500 (Internal Server Error)
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao buscar o canhoto com o ID {id}. Por favor, tente novamente mais tarde.");
+            }
         }
 
         [HttpPost]
         public async Task<ActionResult<LeftHanded>> Cadastrar([FromBody] LeftHanded leftHandedModels)
         {
-            LeftHanded leftHandeds = await _leftHandedRepositorio.Adicionar(leftHandedModels);
+            try
+            {
+                LeftHanded leftHandeds = await _leftHandedRepositorio.Adicionar(leftHandedModels);
 
-            return Ok(leftHandeds);
+                if (leftHandeds == null)
+                {
+                    return NotFound("Canhoto não encontrado");
+                }
+
+                return Ok(leftHandeds);
+            }
+            catch (Exception ex)
+            {
+                // Log da exceção
+                Console.WriteLine($"Ocorreu um erro ao cadastrar o canhoto: {ex.Message}");
+
+                // Retorno de uma resposta de erro com status 500 (Internal Server Error)
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao cadastrar o canhoto. Por favor, tente novamente mais tarde.");
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<CanhotosResponse>> Atualizar([FromBody] LeftHanded leftHandedModels, int id)
         {
-            leftHandedModels.Id = id;
-            CanhotosResponse leftHanded = await _leftHandedRepositorio.Atualizar(leftHandedModels, id);
+            try
+            {
+                leftHandedModels.Id = id;
+                CanhotosResponse leftHanded = await _leftHandedRepositorio.Atualizar(leftHandedModels, id);
+                return Ok(leftHanded);
+            }
+            catch (Exception ex)
+            {
+                // Log da exceção
+                Console.WriteLine($"Ocorreu um erro ao tentar editar o canhoto com o ID {id}: {ex.Message}");
 
-            return Ok(leftHanded);
+                // Retorno de uma resposta de erro com status 500 (Internal Server Error)
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao editar o canhoto com o ID {id}. Por favor, tente novamente mais tarde.");
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<LeftHanded>> Apagar(int id)
         {
-            bool apagado = await _leftHandedRepositorio.Apagar(id);
+            try
+            {
+                bool apagado = await _leftHandedRepositorio.Apagar(id);
 
-            return Ok(apagado);
+                if (!apagado)
+                {
+                    return NotFound("Canhoto não encontrado");
+                }
+
+                return Ok(apagado);
+            }
+            catch (Exception ex)
+            {
+                // Log da exceção
+                Console.WriteLine($"Ocorreu um erro ao tentar apagar o canhoto com o ID {id}: {ex.Message}");
+
+                // Retorno de uma resposta de erro com status 500 (Internal Server Error)
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao apagar o canhoto com o ID {id}. Por favor, tente novamente mais tarde.");
+            }
         }
     }
 }
--- E.CanhotoAPI/Controllers/UserController.cs
+++ E.CanhotoAPI/Controllers/UserController.cs
@@ -1,6 +1,5 @@
 ﻿using E.CanhotoAPI.Models;
 using E.CanhotoAPI.Repositorios.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E.CanhotoAPI.Controllers
@@ -19,40 +18,104 @@ namespace E.CanhotoAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<List<User>>> BuscarTodosOsUsuarios()
         {
-            List<User> users = await _userRepostiorio.BuscarTodosOsUsuarios();
-            return Ok(users);
+            try
+            {
+                List<User> users = await _userRepostiorio.BuscarTodosOsUsuarios();
+                return Ok(users);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao buscar todos os usuários: {ex.Message}");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao buscar todos os usuários. Por favor, tente novamente mais tarde.");
+
+            }
         }
         
         [HttpGet("{id}")]
         public async Task<ActionResult<List<User>>> BuscarPorId(int id)
         {
-            User user = await _userRepostiorio.BuscarPorId(id);
-            return Ok(user);
+            try
+            {
+                User user = await _userRepostiorio.BuscarPorId(id);
+
+                if (user == null)
+                {
+                    return NotFound("Usuario não encontrado");
+                }
+
+                return Ok(user);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao buscar o usuários com o ID {id}: {ex.Message}");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao buscar o usuario que possui o ID {id}");
+            }
+
         } 
 
         [HttpPost]
         public async Task<ActionResult<User>> Cadastrar([FromBody] User userModels)
         {
-            User users = await _userRepostiorio.Adicionar(userModels);
+            try
+            {
+                User users = await _userRepostiorio.Adicionar(userModels);
 
-            return Ok(users);
+                if (User == null)
+                {
+                    return NotFound("Usuario não encontrado");
+                }
+
+                return Ok(users);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao cadastrar o usuario {ex.Message}");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao cadastrar o usuario");
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<User>> Atualizar([FromBody] User userModels, int id)
         {
-            userModels.Id = id;
-            User users = await _userRepostiorio.Atualizar(userModels, id);
+            try
+            {
+                userModels.Id = id;
+                User users = await _userRepostiorio.Atualizar(userModels, id);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao tentar editar o usuario do {id}: {ex.Message}");
 
-            return Ok(users);
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao editarr o usuario");
+            }
+
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<User>> Apagar(int id)
         {
-            bool apagado = await _userRepostiorio.Apagar(id);
+            try
+            {
+                bool apagado = await _userRepostiorio.Apagar(id);
 
-            return Ok(apagado);
+                if (!apagado)
+                {
+                    return NotFound("Usuário não encontrado");
+
+                }
+
+                return Ok(apagado);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao tentar apagar o usuario do {id}: {ex.Message}");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro ao tentar deletar o usuario");
+            }
         }
     }
 }
